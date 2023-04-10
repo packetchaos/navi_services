647e561e4fe47291e064ee7d0c5adbbc6c4ee616
silentninja <cyberdice113@gmail.com>
2023-04-10 11:26:41 -0700

    fixed a spelling error

---
+++ mitre_technique_tags/README.md
@@ -0,0 +1,36 @@
+# Mitre ATT&CK Impact Tags - A docker Service
+
+This project is a Proof of Concept to show how you can use the Tenable API and a SQLite DB to solve some powerful use cases.  
+Wrapping a simple python script into a docker container makes it easy to deploy and reduces deployment risks.  
+It also means you don't need to be a developer or a scripter to take advantage of its power.
+
+# Why?
+
+The github project: ["Mapping MITRE ATT&CK® to CVEs for Impact"](https://github.com/center-for-threat-informed-defense/attack_to_cve) does an amazing job connecting CVEs to impact using Mitre ATT&CK framework.
+The goal with this project is to decorate your Tenable.io assets with this impact data to help you further understand your attack surface and risk.
+
+# How it works
+
+The script parses the CSV created from the above github project and tags each asset with three tags for each CVE.  The three tags are to indicate the advesary behaviors in the picture below.
+The primary impact enables the secondary impact which leads to the exploit technique.
+
+
+[![image](../image.png)
+
+# What does it do?
+
+The script uses the built in tag functionality in navi to tag each asset.
+
+Navi uses a SQLite database and the Tenable.io tag assignments endpoint to accomplish everything below.  To make these dynamic, put the container command or the script on a cronjob or scheduled task.
+
+Note: The Category : Value pair follow the Tag description.  This is what will be seen in Tenable.io should the tag exist.  The Tag is not created if no assets match the navi search.
+
+The script/service tags assets in the below format:
+
+* Mitre : Primary Impact: { Technique ID(s) }
+* Mitre : Secondary Impact: { Technique ID(s) }
+* Mitre : Exploit Technique: { Technique ID(s) }
+
+# Docker command
+    docker run -d {your Access Key} {your secret Key} packetchaos/mitre_tags
+
---
+++ mitre_technique_tags/mitre_tagging.py
@@ -0,0 +1,57 @@
+from os import system as cmd
+import sys
+import time
+import requests
+import pandas as pd
+import io
+
+start = time.time()
+
+access_key = str(sys.argv[1])
+secret_key = str(sys.argv[2])
+
+url = "https://cloud.tenable.com"
+
+# Supporting Document: https://github.com/center-for-threat-informed-defense/attack_to_cve
+csv_url = 'https://raw.githubusercontent.com/center-for-threat-informed-defense/attack_to_cve/master/Att%26ckToCveMappings.csv'
+
+download = requests.get(csv_url).content
+
+data = pd.read_csv(io.StringIO(download.decode('utf-8')))
+
+# Replace 'access_key and secret_key with your keys
+cmd('navi keys --a "{}" --s "{}"'.format(access_key, secret_key))
+
+# Update the navi database for tagging on vulns
+cmd('navi update full')
+
+
+def grab_headers():
+    return {'Content-type': 'application/json', 'user-agent': 'Navi-SS-mitre_tags',
+            'X-ApiKeys': 'accessKey=' + access_key + ';secretKey=' + secret_key}
+
+
+for row in data.values:
+    cveid = row[0]
+    primary_impact = str(row[1])
+    secondary_impact = str(row[2])
+    exploit_technique = str(row[3])
+
+    if "nan" not in primary_impact:
+        print("Tagging assets based on CVE: {} and impact: {} ".format(cveid, primary_impact))
+        cmd('navi tag --c "Mitre" --v "Primary Impact: {}" --cve "{}"'.format(primary_impact, cveid))
+
+    if "nan" not in secondary_impact:
+        print("v assets based on CVE: {} and impact: {} ".format(cveid, secondary_impact))
+        cmd('navi tag --c "Mitre" --v "Secondary Impact: {}" --cve "{}"'.format(secondary_impact, cveid))
+
+    if "nan" not in exploit_technique:
+        print("v assets based on CVE: {} and impact: {} ".format(cveid, exploit_technique))
+        cmd('navi tag --c "Mitre" --v "Exploit Technique: {}" --cve "{}"'.format(exploit_technique, cveid))
+
+finish = time.time()
+
+total = finish - start
+mins = total / 60
+
+print("The Script took {} seconds or {} minutes".format(total, mins))
